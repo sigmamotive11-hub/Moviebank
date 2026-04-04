b3b482e3540874ef3338bb86e3956ce70b56767d
sigmamotive11-hub <sigmamotive11@gmail.com>
2026-04-04 23:35:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static('.'));
+app.use(express.static(path.join(__dirname)));
+
+app.get('/api/videos', require('./api/videos.js'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 module.exports = app;
