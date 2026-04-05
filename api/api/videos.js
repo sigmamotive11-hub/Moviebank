486d29406af29a35f0842747f4e39a04f47fbef7
sigmamotive11-hub <sigmamotive11@gmail.com>
2026-04-05 15:15:48 +0200

    Create videos.js

---
+++ api/api/videos.js
@@ -0,0 +1,18 @@
+const VIDEOS = [
+  {
+    id: '1O6uYZ6APwpLHhoxyERxK8VPvlG5DXMAh',
+    title: 'My First Video',
+    category: 'personal',
+    tags: ['personal'],
+    date: '2024-01-01',
+  },
+];
+
+module.exports = (req, res) => {
+  const videos = VIDEOS.map(v => ({
+    ...v,
+    embedUrl:  `https://drive.google.com/file/d/${v.id}/preview`,
+    thumbnail: `https://drive.google.com/thumbnail?id=${v.id}&sz=w640`,
+  }));
+  res.json({ videos, total: videos.length });
+};
